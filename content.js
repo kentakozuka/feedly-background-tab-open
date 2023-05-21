38fa62845e7c640b02d135ddca1aa1975a606bad
yogo <hashimoto.is.me@gmail.com>
2023-05-22 03:13:34 +0900

    Corresponding changes to site and set up a donation link

--- content.js
+++ content.js
@@ -3,20 +3,19 @@
   chrome.storage.sync.get({ shortcutkey: 'i' }, function (items) {
     shortcutkey = items.shortcutkey;
   });
-
   const keyListener = e => {
     if (!e.repeat && e.key === shortcutkey) {
       let targetURL = null;
 
-      if (document.getElementById('EntryTitleLink-selected') !== null) {
-        targetURL = document.getElementById('EntryTitleLink-selected').href;
-      } else if (e.target.classList.contains('InlineArticle')) {
-        targetURL = e.target.querySelector('.Article__title').href;
-      } else if (document.querySelector('.InlineArticle') !== null) {
+      if (document.querySelector('.EntryTitleLink--selected') !== null) { // Title-Only View(Select by N - P), Magazine View (Select by N - P & Select by J - K), Cards View(Select by N - P & Select by J - K)
+        targetURL = document.querySelector('.EntryTitleLink--selected').href;
+      } else if (document.querySelector('.InlineArticle') !== null) { // Title-Only(Select by J - K), Article View (Select by N - P & Select by J - K)
         targetURL = document.querySelector('.InlineArticle').querySelector('.Article__title').href;
       }
       if (targetURL !== null) {
         chrome.runtime.sendMessage({ url: targetURL });
+      } else {
+        console.log('targetURL is Null');
       }
     }
   };
