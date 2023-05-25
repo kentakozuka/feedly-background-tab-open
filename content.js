d7c4279926c4162d3d35529518c3b69050f6f7ec
yogo <8400185+k-yogo@users.noreply.github.com>
2023-05-25 15:07:49 +0900

    Fixed a bug that did not work in Article View

--- content.js
+++ content.js
@@ -7,9 +7,14 @@
     if (!e.repeat && e.key === shortcutkey) {
       let targetURL = null;
 
-      if (document.querySelector('.EntryTitleLink--selected') !== null) { // Title-Only View(Select by N - P), Magazine View (Select by N - P & Select by J - K), Cards View(Select by N - P & Select by J - K)
+      if (document.querySelector('.EntryTitleLink--selected') !== null) {
+        // ETLS: Title-Only View(Select by N - P), Magazine View, Cards View
         targetURL = document.querySelector('.EntryTitleLink--selected').href;
-      } else if (document.querySelector('.InlineArticle') !== null) { // Title-Only(Select by J - K), Article View (Select by N - P & Select by J - K)
+      } else if (e.target.classList.contains('InlineArticle')) {
+        // AT: Article View
+        targetURL = e.target.querySelector('.Article__title').href;
+      } else if (document.querySelector('.InlineArticle') !== null) {
+        // IA: Title-Only View(Select by J - K)
         targetURL = document.querySelector('.InlineArticle').querySelector('.Article__title').href;
       }
       if (targetURL !== null) {
